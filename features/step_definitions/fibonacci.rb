1ecb1af7853a56f5e4bed484cce6a3eb9f1d1218
Décio Ferreira <decio.ferreira@decioferreira.com>
2013-06-23 13:55:45 +0100

    Fix TypeError check

--- features/step_definitions/fibonacci.rb
+++ features/step_definitions/fibonacci.rb
@@ -3,7 +3,7 @@ Before do
 end
 
 Given(/^I ask for the Fibonacci number of index "(.*?)"$/) do |index|
-  @result = @calculator.calculate(index.to_i)
+  @value = index.to_i
 end
 
 Given(/^I ask the Fibonacci number for the string "(.*?)"$/) do |string|
@@ -11,7 +11,7 @@ Given(/^I ask the Fibonacci number for the string "(.*?)"$/) do |string|
 end
 
 Then(/^I should receive the result of "(.*?)"$/) do |result|
-  assert_equal result.to_i, @result
+  assert_equal result.to_i, @calculator.calculate(@value)
 end
 
 Then(/^I should receive a raise of an error with type "(.*?)"$/) do |arg1|
